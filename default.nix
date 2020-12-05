381044e11f9bb733d136ce30a0d423913645b050
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2020-12-05 10:11:33 -0500

    restore firefox

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
-  # firefox-with-extensions = import ./firefox.nix {
-  #   inherit (pkgs) wrapFirefox firefox-unwrapped fetchFirefoxAddon;
-  # };
-  firefox-with-extensions = pkgs.callPackage ./firefox.nix {};
+  firefox-with-extensions = import ./firefox.nix {
+    inherit (pkgs) wrapFirefox firefox-unwrapped fetchFirefoxAddon;
+  };
   context-vim = pkgs.callPackage ./context-vim.nix {};
   glfw-wayland = pkgs.callPackage ./glfw.nix {};
   gruvbox-gtk = pkgs.callPackage ./gruvbox-gtk.nix {};
