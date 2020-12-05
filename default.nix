76c5f9c3c1c15cbdda4e53c1b5a9be50ff9a5a24
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2020-12-05 10:08:15 -0500

    fix context-vim

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
   firefox-with-extensions = import ./firefox.nix {
     inherit (pkgs) wrapFirefox firefox-unwrapped fetchFirefoxAddon;
   };
-  context-vim = pkgs.callPackage ./context-vim.nix { inherit pkgs; };
+  context-vim = pkgs.callPackage ./context-vim.nix {};
   glfw-wayland = pkgs.callPackage ./glfw.nix {};
   gruvbox-gtk = pkgs.callPackage ./gruvbox-gtk.nix {};
   gruvbox-icons = pkgs.callPackage ./gruvbox-icons.nix {};
