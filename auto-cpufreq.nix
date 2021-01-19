67b9485f68a4ec6994c11f098c5f8ef7b6416efc
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-01-19 08:26:19 -0500

    update auto-cpufreq

--- auto-cpufreq.nix
+++ auto-cpufreq.nix
@@ -7,8 +7,8 @@ python3Packages.buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Technical27";
     repo = pname;
-    rev = "bd40bc98ff82a5042e34b34a7e488f52a66de373";
-    sha256 = "sha256-ovQWi3n1OmI5JWNWkSTTdf5hukO3f8/ol7m8JHYrL7w=";
+    rev = "2fb43b0df7cff3fe687c8a8447838fe4d1f6d093";
+    sha256 = "sha256-gyRfYF4yOMbufmer11XjsaWK/RDBu+fYyvcsMY0h4tw=";
   };
 
   doCheck = false;
@@ -16,7 +16,7 @@ python3Packages.buildPythonPackage rec {
 
   propagatedBuildInputs = with python3Packages; [ click distro psutil ];
 
-  patches = [ ./prevent-install-and-copy-updated.patch ];
+  patches = [ ./prevent-install-and-copy.patch ];
 
   postInstall = ''
     cp ${src}/scripts/cpufreqctl.sh $out/bin/cpufreqctl
