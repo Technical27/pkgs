c5675d79d3b6e04eedc6d1ecf2a904ccc2c79f4e
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-01-21 13:23:28 -0500

    update auto-cpufreq fork

--- auto-cpufreq.nix
+++ auto-cpufreq.nix
@@ -7,19 +7,19 @@ python3Packages.buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Technical27";
     repo = pname;
-    rev = "2fb43b0df7cff3fe687c8a8447838fe4d1f6d093";
-    sha256 = "sha256-gyRfYF4yOMbufmer11XjsaWK/RDBu+fYyvcsMY0h4tw=";
+    rev = "cb6ac6ad076fb6fca21acf30c2467d372e6ca077";
+    sha256 = "sha256-8rYNu/SP82YQ5YOb1qgP55wDFBZK4eiisRSjywcQCjg=";
   };
 
   doCheck = false;
-  pythonImportsCheck = [ "source" ];
+  pythonImportsCheck = [ "auto_cpufreq" ];
 
   propagatedBuildInputs = with python3Packages; [ click distro psutil ];
 
   patches = [ ./prevent-install-and-copy.patch ];
 
   postInstall = ''
-    cp ${src}/scripts/cpufreqctl.sh $out/bin/cpufreqctl
+    cp ${src}/scripts/cpufreqctl.sh $out/bin/cpufreqctl.auto-cpufreq
 
     mkdir -p $out/lib/systemd/system
     cp ${src}/scripts/auto-cpufreq.service $out/lib/systemd/system
