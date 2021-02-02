e1908d1bceeee486787ce438e05225ee9631d938
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-02-02 12:53:09 -0500

    update wgvpn

--- wgvpn.fish
+++ wgvpn.fish
@@ -3,19 +3,22 @@ if test (id -u) = 0
   exit 1
 end
 
+ip rule list | rg "lookup 1000"
+set IsConnected $status
+
 switch $argv[1]
   case up
-    if test ! -f /tmp/.wgvpn
+    if test $IsConnected -eq 1
       sudo ip rule add not from all fwmark 51000 lookup 1000
-      touch /tmp/.wgvpn
+      sudo ip -6 rule add not from all fwmark 51000 lookup 1000
     else
       echo "already up"
       exit 1
     end
   case down
-    if test -f /tmp/.wgvpn
+    if test $IsConnected -eq 0
       sudo ip rule delete not from all fwmark 51000 lookup 1000
-      rm /tmp/.wgvpn
+      sudo ip -6 rule delete not from all fwmark 51000 lookup 1000
     else
       echo "not up"
       exit 1
