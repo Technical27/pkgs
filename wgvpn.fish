854e1010cf47fdce12430b4a3b1a5c29db6419f5
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-02-05 14:25:32 -0500

    update wgvpn

--- wgvpn.fish
+++ wgvpn.fish
@@ -1,28 +1,40 @@
-if test (id -u) = 0
-  echo "don't run this script as root, it will call sudo"
-  exit 1
+function sudo
+  if test (id -u) -eq 0
+    $argv
+  else
+    command sudo $argv
+  end
 end
 
-ip rule list | rg "lookup 1000"
-set IsConnected $status
+
+# jq expression to parse ip -j
+set jq_expr '
+[
+.[] |
+.fwmark == "0xc738" and .src == "all" and .table == "1000" and .not == null
+] | any
+'
+
+set connected_ipv4 ip -4 -j rule list | jq $jq_expr
+set connected_ipv6 ip -6 -j rule list | jq $jq_expr
 
 switch $argv[1]
   case up
-    if test $IsConnected -eq 1
-      sudo ip rule add not from all fwmark 51000 lookup 1000
-      sudo ip -6 rule add not from all fwmark 51000 lookup 1000
-    else
-      echo "already up"
-      exit 1
+    if test $connected_ipv4 -eq "false"
+      sudo ip rule add not from all fwmark 0xc738 lookup 1000
+    end
+    if test $connected_ipv6 -eq "false"
+      sudo ip -6 rule add not from all fwmark 0xc738 lookup 1000
     end
   case down
-    if test $IsConnected -eq 0
-      sudo ip rule delete not from all fwmark 51000 lookup 1000
-      sudo ip -6 rule delete not from all fwmark 51000 lookup 1000
-    else
-      echo "not up"
-      exit 1
+    if test $connected_ipv4 -eq "true"
+      sudo ip rule delete not from all fwmark 0xc738 lookup 1000
     end
+    if test $connected_ipv4 -eq "true"
+      sudo ip -6 rule delete not from all fwmark 0xc738 lookup 1000
+    end
+  case status
+    echo "ipv4: $connected_ipv4, ipv6: $connected_ipv6"
   case '*'
-    echo "use 'up' or 'down' to activate/deactivate the wireguard vpn"
+    echo "use 'up' or 'down' to activate/deactivate the wireguard vpn or 'status ' to check"
 end
