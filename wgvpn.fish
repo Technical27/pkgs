08c72a8e74043b8b223237948b5c74504eb04585
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-02-05 22:34:59 -0500

    update wgvpn

--- wgvpn.fish
+++ wgvpn.fish
@@ -1,39 +1,38 @@
-function sudo
-  if test (id -u) -eq 0
-    $argv
-  else
-    command sudo $argv
-  end
+if test (id -u) -eq 0
+  echo "don't run this script as root, it calls sudo"
+  exit 1
 end
 
+set wg_fwmark 0xc738
+set wg_table 1000
+set wg_rule not from all fwmark $wg_fwmark lookup $wg_table
 
 # jq expression to parse ip -j
-set jq_expr '
-[
+set jq_expr \
+"[
 .[] |
-.fwmark == "0xc738" and .src == "all" and .table == "1000" and .not == null
-] | any
-'
+.fwmark == \"$wg_fwmark\" and .src == \"all\" and .table == \"$wg_table\" and .not == null
+] | any"
 
 set connected_ipv4 (ip -4 -j rule list | jq $jq_expr)
 set connected_ipv6 (ip -6 -j rule list | jq $jq_expr)
 
 switch $argv[1]
   case up
-    sudo resolvectl domain wg0 '~.'
+    sudo resolvectl domain wg0 "~."
     if test $connected_ipv4 = "false"
-      sudo ip -4 rule add not from all fwmark 0xc738 lookup 1000
+      sudo ip -4 rule add $wg_rule
     end
     if test $connected_ipv6 = "false"
-      sudo ip -6 rule add not from all fwmark 0xc738 lookup 1000
+      sudo ip -6 rule add $wg_rule
     end
   case down
-    sudo resolvectl domain wg0 ''
+    sudo resolvectl domain wg0 ""
     if test $connected_ipv4 = "true"
-      sudo ip -4 rule delete not from all fwmark 0xc738 lookup 1000
+      sudo ip -4 rule delete $wg_rule
     end
     if test $connected_ipv4 = "true"
-      sudo ip -6 rule delete not from all fwmark 0xc738 lookup 1000
+      sudo ip -6 rule delete $wg_rule
     end
   case status
     if test $connected_ipv4 = $connected_ipv6
@@ -46,9 +45,9 @@ switch $argv[1]
       if test $connected_ipv4 = "true"
         echo "connected only on ipv4"
       else
-        echo "connected on ipv6"
+        echo "connected only on ipv6"
       end
     end
-  case '*'
-    echo "use 'up' or 'down' to activate/deactivate the wireguard vpn or 'status ' to check"
+  case "*"
+    echo "use 'up' or 'down' to activate/deactivate the wireguard vpn or 'status' to check"
 end
