7b44d1b36a99f41344dcdd5c5ca0ceac6e1b96fb
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-07-15 09:12:18 -0400

    update grapejuice

--- outputs/games/roblox/default.nix
+++ outputs/games/roblox/default.nix
@@ -8,9 +8,20 @@ rec {
       patches = old.patches ++ [ ./mouse_fix.patch ];
     }
   );
-  grapejuice = prev.grapejuice.override {
-    inherit wine;
-  };
+  grapejuice = (
+    prev.grapejuice.override {
+      inherit wine;
+    }
+  ).overrideAttrs (
+    old: {
+      src = prev.fetchFromGitLab {
+        owner = "brinkervii";
+        repo = "grapejuice";
+        rev = "61f9cb33b00659c0bac5c7dc73275eb1deeb987a";
+        sha256 = "sha256-LbM4HF6M4eRtJJstsz7QnUWF+V/lW8nSYLP0F2Kds64=";
+      };
+    }
+  );
   rbxfpsunlocker = prev.callPackage ./rbxfpsunlocker.nix {
     inherit wine;
   };
