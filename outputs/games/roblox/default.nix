81dda28750994d54b6567bea7ee10dad60f30b50
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-08-16 10:04:56 -0400

    nixpkgs update / update grapejuice

--- outputs/games/roblox/default.nix
+++ outputs/games/roblox/default.nix
@@ -1,27 +1,10 @@
 final: prev:
 
 rec {
-  wine = prev.lib.overrideDerivation prev.wineWowPackages.staging (
-    old: {
-      # roblox will freeze the cursor on any camera change
-      # this is a patch to prevent that
-      patches = old.patches ++ [ ./mouse_fix.patch ];
-    }
-  );
-  grapejuice = (
-    prev.grapejuice.override {
-      inherit wine;
-    }
-  ).overrideAttrs (
-    old: {
-      src = prev.fetchFromGitLab {
-        owner = "brinkervii";
-        repo = "grapejuice";
-        rev = "61f9cb33b00659c0bac5c7dc73275eb1deeb987a";
-        sha256 = "sha256-LbM4HF6M4eRtJJstsz7QnUWF+V/lW8nSYLP0F2Kds64=";
-      };
-    }
-  );
+  wine = final.wineWowPackages.staging;
+  grapejuice = prev.grapejuice.override {
+    inherit wine;
+  };
   rbxfpsunlocker = prev.callPackage ./rbxfpsunlocker.nix {
     inherit wine;
   };
