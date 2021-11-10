20a4b6f2ed9040810be03035e20b979eb0b576cf
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-11-10 18:41:46 -0500

    remove dumb naming

--- flake.nix
+++ flake.nix
@@ -15,61 +15,57 @@
           icons = prev.callPackage ./outputs/gruvbox.nix { icons = true; };
         };
 
-        tools = {
-          pros = import ./outputs/tools/pros { pkgs = final; };
-          info = (import ./outputs/tools/info { pkgs = prev; }).package;
-          wgvpn = prev.callPackage ./outputs/tools/wgvpn { };
-          polybar = prev.polybar.override { i3GapsSupport = true; };
-          glfw-wayland = prev.glfw-wayland.overrideAttrs (
-            old: {
-              patches = old.patches ++ [
-                # ./outputs/tools/glfw/0001-set-O_NONBLOCK-on-repeat-timerfd.patch already included
-                ./outputs/tools/glfw/0002-Don-t-crash-on-calls-to-focus-or-icon.patch
-                ./outputs/tools/glfw/0003-fix-broken-opengl-screenshots-on-mutter.patch
-                ./outputs/tools/glfw/0004-Do-not-crash-on-window-position-set.patch
-              ];
-            }
-          );
-          tree-sitter = prev.tree-sitter.overrideAttrs (
-            old: {
-              src = prev.fetchFromGitHub {
-                owner = "tree-sitter";
-                repo = "tree-sitter";
-                rev = "cd96552448a6e0d4eb27fc54b27cb5130c4b6f76";
-                sha256 = "sha256-l2XiqyGe5dwvDxM32xSSl8caUyAuNsVfRpAQZ/B5M9U=";
-              };
-            }
-          );
-          mangohud = prev.callPackage ./outputs/tools/mangohud {
-            libXNVCtrl = final.linuxPackages.nvidia_x11_beta.settings.libXNVCtrl;
-            mangohud32 = final.pkgsi686Linux.cpkgs.tools.mangohud;
-          };
-          cemu = prev.libsForQt5.callPackage ./outputs/tools/cemu.nix { };
-          wlroots = prev.callPackage ./outputs/tools/wlroots.nix { };
-          firebird = prev.libsForQt5.callPackage ./outputs/tools/firebird.nix { };
-          joplin = prev.joplin-desktop.overrideAttrs (old: rec {
-            version = "2.5.10";
-            src = prev.fetchurl {
-              url = "https://github.com/laurent22/joplin/releases/download/v${version}/Joplin-${version}.AppImage";
-              sha256 = "sha256-636/SifcawS1fdsrSCAASvT147EKn02IXN7DBZRfXME=";
+        pros = import ./outputs/pros { pkgs = final; };
+        info = (import ./outputs/info { pkgs = prev; }).package;
+        wgvpn = prev.callPackage ./outputs/wgvpn { };
+        polybar = prev.polybar.override { i3GapsSupport = true; };
+        glfw-wayland = prev.glfw-wayland.overrideAttrs (
+          old: {
+            patches = old.patches ++ [
+              # ./outputs/glfw/0001-set-O_NONBLOCK-on-repeat-timerfd.patch already included
+              ./outputs/glfw/0002-Don-t-crash-on-calls-to-focus-or-icon.patch
+              ./outputs/glfw/0003-fix-broken-opengl-screenshots-on-mutter.patch
+              ./outputs/glfw/0004-Do-not-crash-on-window-position-set.patch
+            ];
+          }
+        );
+        tree-sitter = prev.tree-sitter.overrideAttrs (
+          old: {
+            src = prev.fetchFromGitHub {
+              owner = "tree-sitter";
+              repo = "tree-sitter";
+              rev = "cd96552448a6e0d4eb27fc54b27cb5130c4b6f76";
+              sha256 = "sha256-l2XiqyGe5dwvDxM32xSSl8caUyAuNsVfRpAQZ/B5M9U=";
             };
-          });
-          autovpn = prev.callPackage ./outputs/tools/autovpn { };
+          }
+        );
+        mangohud = prev.callPackage ./outputs/mangohud {
+          libXNVCtrl = final.linuxPackages.nvidia_x11_beta.settings.libXNVCtrl;
+          mangohud32 = final.pkgsi686Linux.cpkgs.tools.mangohud;
         };
+        cemu = prev.libsForQt5.callPackage ./outputs/cemu.nix { };
+        wlroots = prev.callPackage ./outputs/wlroots.nix { };
+        firebird = prev.libsForQt5.callPackage ./outputs/firebird.nix { };
+        joplin = prev.joplin-desktop.overrideAttrs (old: rec {
+          version = "2.5.10";
+          src = prev.fetchurl {
+            url = "https://github.com/laurent22/joplin/releases/download/v${version}/Joplin-${version}.AppImage";
+            sha256 = "sha256-636/SifcawS1fdsrSCAASvT147EKn02IXN7DBZRfXME=";
+          };
+        });
+        autovpn = prev.callPackage ./outputs/autovpn { };
 
-        games = {
-          # TODO: maybe add this again for shapez.io
-          # steam = prev.steam.override { extraPkgs = pkgs: with pkgs; [ mesa sqlite ]; };
-          guilded = prev.callPackage ./outputs/games/guilded.nix { };
-          badlion-client = prev.callPackage ./outputs/games/badlion.nix { };
-          roblox.grapejuice = (
-            prev.grapejuice.override {
-              wine = final.wineWowPackages.staging;
-            }
-          );
-          gamescope = prev.callPackage ./outputs/games/gamescope.nix { };
-          grapejuice = prev.callPackage ./outputs/games/roblox { };
-        };
+        # TODO: maybe add this again for shapez.io
+        # steam = prev.steam.override { extraPkgs = pkgs: with pkgs; [ mesa sqlite ]; };
+        guilded = prev.callPackage ./outputs/guilded.nix { };
+        badlion-client = prev.callPackage ./outputs/badlion.nix { };
+        roblox.grapejuice = (
+          prev.grapejuice.override {
+            wine = final.wineWowPackages.staging;
+          }
+        );
+        gamescope = prev.callPackage ./outputs/gamescope.nix { };
+        grapejuice = prev.callPackage ./outputs/roblox { };
       };
     };
 
@@ -84,5 +80,7 @@
     nixosModule = { ... }: {
       nixpkgs.overlays = [ self.overlay ];
     };
+
+    hydraJobs.x86_64-linux = self.packages.x86_64-linux;
   };
 }
