07ad0664340378801c2ac6a0e29bd65b1d52e032
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-11-10 10:53:49 -0500

    more changes

--- outputs/tools/wgvpn/wgvpn.fish
+++ outputs/tools/wgvpn/wgvpn.fish
@@ -17,6 +17,44 @@ set jq_expr \
 set connected_ipv4 (ip -4 -j rule list | jq $jq_expr)
 set connected_ipv6 (ip -6 -j rule list | jq $jq_expr)
 
+function check_status_ipv4
+  set connected_ipv4 (ip -4 -j rule list | jq $jq_expr)
+  if test $connected_ipv4 = "true"
+    return 1
+  else
+    return 0
+  end
+end
+
+function check_status_ipv6
+  set connected_ipv6 (ip -6 -j rule list | jq $jq_expr)
+  if test $connected_ipv6 = "true"
+    return 1
+  else
+    return 0
+  end
+end
+
+function check_status
+  check_status_ipv4
+  set connected_ipv4 $status
+  check_status_ipv6
+  set connected_ipv6 $status
+  if test $connected_ipv4 = $connected_ipv6
+    if test $connected_ipv4 = 1
+      return 0
+    else
+      return 1
+    end
+  else
+    if test $connected_ipv4 = 1
+      return 3
+    else
+      return 4
+    end
+  end
+end
+
 switch $argv[1]
   case up
     sudo resolvectl domain wg0 "~."
@@ -35,21 +73,28 @@ switch $argv[1]
       sudo ip -6 rule delete $wg_rule
     end
   case status
-    if test $connected_ipv4 = $connected_ipv6
-      if test $connected_ipv4 = "true"
+    check_status
+    set vpn_status $status
+    switch $vpn_status
+      case 0
         echo "vpn connected"
-        exit 0
-      else
+      case 1
         echo "vpn disconnected"
-        exit 1
-      end
-    else
-      if test $connected_ipv4 = "true"
-        echo "connected only on ipv4"
+      case 2
+        echo "vpn only connected on ipv4"
+      case 3
+        echo "vpn only connected on ipv6"
+    end
+  case bar
+    while true
+      check_status
+      set vpn_status $status
+      if test $status = 0
+        echo '{\"class\": \"connected\", \"text\": \"VPN \"}'
       else
-        echo "connected only on ipv6"
+        echo '{\"class\": \"disconnected\", \"text\": \"VPN \"}'
       end
-      exit 1
+      sleep 10
     end
   case "*"
     echo "use 'up' or 'down' to activate/deactivate the wireguard vpn or 'status' to check"
