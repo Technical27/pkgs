78e2cb114f9dbd8cd2f1eafce72ac96da77e0c35
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-11-10 19:10:17 -0500

    fix guilded hash

--- outputs/guilded.nix
+++ outputs/guilded.nix
@@ -1,10 +1,47 @@
-{ fetchurl, dpkg
-, autoPatchelfHook, makeDesktopItem, lib, stdenv, wrapGAppsHook
-, alsaLib, at-spi2-atk, at-spi2-core, atk, cairo, cups, dbus, expat, fontconfig
-, freetype, gdk-pixbuf, glib, gtk3, libcxx, libdrm, libnotify, libpulseaudio, libuuid
-, libX11, libXScrnSaver, libXcomposite, libXcursor, libXdamage, libXext
-, libXfixes, libXi, libXrandr, libXrender, libXtst, libxcb
-, mesa, nspr, nss, pango, systemd, libappindicator-gtk3, libdbusmenu
+{ fetchurl
+, dpkg
+, autoPatchelfHook
+, makeDesktopItem
+, lib
+, stdenv
+, wrapGAppsHook
+, alsaLib
+, at-spi2-atk
+, at-spi2-core
+, atk
+, cairo
+, cups
+, dbus
+, expat
+, fontconfig
+, freetype
+, gdk-pixbuf
+, glib
+, gtk3
+, libcxx
+, libdrm
+, libnotify
+, libpulseaudio
+, libuuid
+, libX11
+, libXScrnSaver
+, libXcomposite
+, libXcursor
+, libXdamage
+, libXext
+, libXfixes
+, libXi
+, libXrandr
+, libXrender
+, libXtst
+, libxcb
+, mesa
+, nspr
+, nss
+, pango
+, systemd
+, libappindicator-gtk3
+, libdbusmenu
 }:
 
 stdenv.mkDerivation rec {
@@ -12,7 +49,7 @@ stdenv.mkDerivation rec {
   version = "unstable-2021-05-18";
   src = fetchurl {
     url = "https://www.guilded.gg/downloads/Guilded-Linux.deb";
-    sha256 = "14d1bb2cf0cng8md2c4x63wjj34fjrb5jjq8l0774iylz7yzgs0g";
+    sha256 = "sha256-D80ZDxK8fNr3sIU1IQXHdOpSErCc0aNKDFQnlXzP5do=";
   };
 
   nativeBuildInputs = [
@@ -41,16 +78,47 @@ stdenv.mkDerivation rec {
   dontWrapGApps = true;
 
   libPath = lib.makeLibraryPath [
-    libcxx systemd libpulseaudio
-    stdenv.cc.cc alsaLib atk at-spi2-atk at-spi2-core cairo cups dbus expat fontconfig freetype
-    gdk-pixbuf glib gtk3 libnotify libX11 libXcomposite libuuid
-    libXcursor libXdamage libXext libXfixes libXi libXrandr libXrender
-    libXtst nspr nss libxcb pango systemd libXScrnSaver
-    libappindicator-gtk3 libdbusmenu
+    libcxx
+    systemd
+    libpulseaudio
+    stdenv.cc.cc
+    alsaLib
+    atk
+    at-spi2-atk
+    at-spi2-core
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libnotify
+    libX11
+    libXcomposite
+    libuuid
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    nspr
+    nss
+    libxcb
+    pango
+    systemd
+    libXScrnSaver
+    libappindicator-gtk3
+    libdbusmenu
   ];
 
   # for some reason it can't find libxkbcommon-x11.so.0 even though it can find libxkbcommon.so.0
-  autoPatchelfIgnoreMissingDeps=true;
+  autoPatchelfIgnoreMissingDeps = true;
 
   installPhase = ''
     mkdir -p $out/{opt,bin}
@@ -66,7 +134,7 @@ stdenv.mkDerivation rec {
     ln -s $out/opt/Guilded/guilded $out/bin/
     substituteInPlace $out/share/applications/guilded.desktop --replace "/opt/Guilded/guilded" guilded
   '';
-    # ln -s "${desktopItem}/share/applications" $out/share/
+  # ln -s "${desktopItem}/share/applications" $out/share/
 
   # desktopItem = makeDesktopItem {
   #   name = pname;
