9e6883f522257a4371bc21c053a48d60957252ac
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-12-07 20:37:08 -0500

    add nvidia overlay

--- flake.nix
+++ flake.nix
@@ -4,64 +4,99 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    overlay = final: prev: {
-      cpkgs = {
-        gruvbox = {
-          theme = prev.callPackage ./outputs/gruvbox.nix { };
-          icons = prev.callPackage ./outputs/gruvbox.nix { icons = true; };
-        };
 
-        pros = prev.callPackage ./outputs/pros { };
-        info = (import ./outputs/info { pkgs = prev; }).package;
-        wgvpn = prev.callPackage ./outputs/wgvpn { };
-        polybar = prev.polybar.override { i3GapsSupport = true; };
-        glfw-wayland = prev.glfw-wayland.overrideAttrs (
-          old: {
-            patches = [
-              ./outputs/glfw/0002-Don-t-crash-on-calls-to-focus-or-icon.patch
-              ./outputs/glfw/0003-fix-broken-opengl-screenshots-on-mutter.patch
-              ./outputs/glfw/0004-Do-not-crash-on-window-position-set.patch
-              # NOTE: This is now a file instead of a list...
-              old.patches
-            ];
-          }
-        );
-        cemu = prev.libsForQt5.callPackage ./outputs/cemu.nix { };
-        firebird = prev.libsForQt5.callPackage ./outputs/firebird.nix { };
-        joplin = prev.joplin-desktop.overrideAttrs (old: rec {
-          version = "2.5.10";
-          src = prev.fetchurl {
-            url = "https://github.com/laurent22/joplin/releases/download/v${version}/Joplin-${version}.AppImage";
-            sha256 = "sha256-636/SifcawS1fdsrSCAASvT147EKn02IXN7DBZRfXME=";
+    overlays = {
+      main = final: prev: {
+        cpkgs = {
+          gruvbox = {
+            theme = prev.callPackage ./outputs/gruvbox.nix { };
+            icons = prev.callPackage ./outputs/gruvbox.nix { icons = true; };
           };
-        });
-        autovpn = prev.callPackage ./outputs/autovpn { };
 
-        # TODO: maybe add this again for shapez.io
-        # steam = prev.steam.override { extraPkgs = pkgs: with pkgs; [ mesa sqlite ]; };
-        guilded = prev.callPackage ./outputs/guilded.nix { };
-        badlion-client = prev.callPackage ./outputs/badlion.nix { };
-        grapejuice = prev.callPackage ./outputs/roblox { };
-        gamescope = prev.callPackage ./outputs/gamescope { };
+          pros = prev.callPackage ./outputs/pros { };
+          info = (import ./outputs/info { pkgs = prev; }).package;
+          wgvpn = prev.callPackage ./outputs/wgvpn { };
+          polybar = prev.polybar.override { i3GapsSupport = true; };
+          glfw-wayland = prev.glfw-wayland.overrideAttrs (
+            old: {
+              patches = [
+                ./outputs/glfw/0002-Don-t-crash-on-calls-to-focus-or-icon.patch
+                ./outputs/glfw/0003-fix-broken-opengl-screenshots-on-mutter.patch
+                ./outputs/glfw/0004-Do-not-crash-on-window-position-set.patch
+                # NOTE: This is now a file instead of a list...
+                old.patches
+              ];
+            }
+          );
+          cemu = prev.libsForQt5.callPackage ./outputs/cemu.nix { };
+          firebird = prev.libsForQt5.callPackage ./outputs/firebird.nix { };
+          joplin = prev.joplin-desktop.overrideAttrs (old: rec {
+            version = "2.5.10";
+            src = prev.fetchurl {
+              url = "https://github.com/laurent22/joplin/releases/download/v${version}/Joplin-${version}.AppImage";
+              sha256 = "sha256-636/SifcawS1fdsrSCAASvT147EKn02IXN7DBZRfXME=";
+            };
+          });
+          autovpn = prev.callPackage ./outputs/autovpn { };
 
-        vscodium = prev.callPackage ./outputs/vscodium.nix { };
+          # TODO: maybe add this again for shapez.io
+          # steam = prev.steam.override { extraPkgs = pkgs: with pkgs; [ mesa sqlite ]; };
+          guilded = prev.callPackage ./outputs/guilded.nix { };
+          badlion-client = prev.callPackage ./outputs/badlion.nix { };
+          grapejuice = prev.callPackage ./outputs/roblox { };
+          gamescope = prev.callPackage ./outputs/gamescope { };
 
-        wine-fusion360 = prev.callPackage ./outputs/wine-fusion360 { };
+          vscodium = prev.callPackage ./outputs/vscodium.nix { };
 
-        robotmeshnative = prev.callPackage ./outputs/robotmeshnative.nix { };
+          wine-fusion360 = prev.callPackage ./outputs/wine-fusion360 { };
+
+          robotmeshnative = prev.callPackage ./outputs/robotmeshnative.nix { };
+
+          wlroots = prev.wlroots.overrideAttrs (old: {
+            postPatch = ''
+              sed -i 's/assert(argb8888 &&/assert(true || argb8888 ||/g' 'render/wlr_renderer.c'
+            '';
+          });
+          egl-wayland = prev.egl-wayland.overrideAttrs (old: rec {
+            version = "1.1.9.99";
+            src = prev.fetchFromGitHub {
+              owner = "NVIDIA";
+              repo = "egl-wayland";
+              rev = "daab8546eca8428543a4d958a2c53fc747f70672"; # Oct 29 2021
+              sha256 = "sha256-IrLeqBW74mzo2OOd5GzUPDcqaxrsoJABwYyuKTGtPsw=";
+            };
+            buildInputs = old.buildInputs ++ [ prev.wayland-protocols ];
+          });
+          libglvnd = prev.libglvnd.overrideAttrs (old: rec {
+            version = "1.3.4.99";
+            src = prev.fetchFromGitLab {
+              domain = "gitlab.freedesktop.org";
+              owner = "glvnd";
+              repo = "libglvnd";
+              rev = "2d69d4720c56d2d8ab1f81eff62eecd069f14c62"; # Oct 28 2021
+              sha256 = "sha256-137gLX7LgfOBNnci9rnp4fg194m3ieY2q9HuRhQdb1Y=";
+            };
+          });
+        };
+      };
+
+      nvidia = final: prev: {
+        wlroots = final.cpkgs.wlroots;
+        egl-wayland = final.cpkgs.egl-wayland;
+        libglvnd = final.cpkgs.libglvnd;
       };
     };
 
     packages.x86_64-linux = (
       import nixpkgs {
         system = "x86_64-linux";
-        overlays = [ self.overlay ];
+        overlays = [ self.overlays.main ];
         config.allowUnfree = true;
       }
     ).cpkgs;
 
     nixosModule = { ... }: {
-      nixpkgs.overlays = [ self.overlay ];
+      nixpkgs.overlays = [ self.overlays.main ];
     };
 
     hydraJobs.x86_64-linux = self.packages.x86_64-linux;
