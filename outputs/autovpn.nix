df14bfb991da98471e9ad9e423c4841c3b76d417
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2022-10-23 14:07:48 -0400

    nixpkgs and autovpn update

--- outputs/autovpn.nix
+++ outputs/autovpn.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "autovpn";
-  version = "unstable-2022-03-31";
+  version = "unstable-2022-10-23";
 
   src = fetchFromGitHub {
     owner = "Technical27";
     repo = pname;
-    rev = "f4aa18d95d282d472ea935aea63951eac28f008b";
-    sha256 = "sha256-iRXuIMU7Saw4R18wH32is22yltpVEKzqYjiMiP6EAAk=";
+    rev = "2161639021565fe4c31a54405dff4d7444274acf";
+    sha256 = "sha256-/LiTjqhnS+jV3QeMoEI/CrnD0ivlO3ECWqkwef9p2N8=";
   };
 
-  cargoSha256 = "sha256-RR6Z4oCP3bX7gvyzu59UJwbfKu3IsaWTMWECxyZCwFg=";
+  cargoSha256 = "sha256-f/5KbDkgOLEtTfWRqRrwEcBv0f9m74kdoNdwVuFvqu8=";
 
   nativeBuildInputs = [
     pkg-config
